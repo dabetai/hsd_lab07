e6fb420660a0abbf75dea5b3df12176ab3740f22
dabetai <sangmin@DESKTOP-UIUAC05.localdomain>
2020-05-05 03:36:24 +0900

    1st mod

--- src/fpga_api.cpp
+++ src/fpga_api.cpp
@@ -143,7 +143,7 @@ void FPGA::convLowering(const std::vector<std::vector<std::vector<std::vector<fl
       for(int chan = 0; chan < input_channel; chan ++) {
         for(int conv_i = 0; conv_i < conv_height; conv_i ++) {
           for(int conv_j = 0; conv_j < conv_width; conv_j ++) {
-            new_inputs[new_inputs_i ++][new_inputs_j] = inputs[chan][filter_i + conv_i][filter_j + conv_j];
+            new_inputs[new_inputs_i ++][new_inputs_j] = cnn_weights[chan][filter_i + conv_i][filter_j + conv_j];
           }
         }
       }
